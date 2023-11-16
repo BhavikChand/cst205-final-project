d7788c34773af6c48c96219643397d9c5583414d
The-MC-0 <113734505+The-MC-0@users.noreply.github.com>
2023-11-15 17:14:12 -0800

    Add files via upload
    gui version 2

--- final.py
+++ final.py
@@ -1,6 +1,6 @@
 #Author: Michael Conley
 #Class: CST 205
-#Date: 11/8/2023
+#Date: 11/15/2023
 #Description: Creates a window that takes 2 file names, an xy coordinate, and blend strengths and manipulations for both images, then places the second image on the first according to the information given.
 import sys
 from PySide6.QtWidgets import (QWidget, QApplication, QLabel, QVBoxLayout, QLineEdit, QComboBox, QPushButton)
@@ -31,17 +31,21 @@ class MyWindow(QWidget):
         self.y_le_lbl = QLabel('Enter subject image y coordinate:')
         self.my_y_le = QLineEdit("")
         self.my_y_le.minimum_width = 250
+        #The image will be placed with the top left corner on the given pixel
 
         self.bg_blend_le_lbl = QLabel('Enter background blend strength:')
         self.my_bg_blend_le = QLineEdit("")
         self.my_bg_blend_le.minimum_width = 250
+        #blending from the edge of the subject into the background
 
         self.s_blend_le_lbl = QLabel('Enter subject blend strength:')
         self.my_s_blend_le = QLineEdit("")
         self.my_s_blend_le.minimum_width = 250
+        #blending from the edge of the subject into the subject
+
+        self.my_list = ["None", "Sepia", "Negative", "Grayscale", "Thumbnail", "Chromatic Abberation"]
 
         self.bg_cb_lbl = QLabel('Select background manipulation:')
-        self.my_list = ["None", "Sepia", "Negative", "Grayscale", "Thumbnail"]
         self.my_bg_combo_box = QComboBox()
         self.my_bg_combo_box.add_items(self.my_list)
 
@@ -49,6 +53,23 @@ class MyWindow(QWidget):
         self.my_s_combo_box = QComboBox()
         self.my_s_combo_box.add_items(self.my_list)
 
+        self.bg_w_le_lbl = QLabel('Enter background width (0 for default):')
+        self.my_bg_w_le = QLineEdit("0")
+        self.my_bg_w_le.minimum_width = 250
+        
+        self.bg_h_le_lbl = QLabel('Enter background height (0 for default):')
+        self.my_bg_h_le = QLineEdit("0")
+        self.my_bg_h_le.minimum_width = 250
+
+        self.s_w_le_lbl = QLabel('Enter subject width (0 for default):')
+        self.my_s_w_le = QLineEdit("0")
+        self.my_s_w_le.minimum_width = 250
+        
+        self.s_h_le_lbl = QLabel('Enter subject height (0 for default):')
+        self.my_s_h_le = QLineEdit("0")
+        self.my_s_h_le.minimum_width = 250
+        #negative sizes will mean the image will be flipped
+        
         my_btn = QPushButton("Submit")
 
         my_btn.clicked.connect(self.on_submit)
@@ -69,6 +90,14 @@ class MyWindow(QWidget):
         vbox.add_widget(self.my_bg_combo_box)
         vbox.add_widget(self.s_cb_lbl)
         vbox.add_widget(self.my_s_combo_box)
+        vbox.add_widget(self.bg_w_le_lbl)
+        vbox.add_widget(self.my_bg_w_le)
+        vbox.add_widget(self.bg_h_le_lbl)
+        vbox.add_widget(self.my_bg_h_le)
+        vbox.add_widget(self.s_w_le_lbl)
+        vbox.add_widget(self.my_s_w_le)
+        vbox.add_widget(self.s_h_le_lbl)
+        vbox.add_widget(self.my_s_h_le)
 
         vbox.add_widget(my_btn)
 
@@ -84,7 +113,11 @@ class MyWindow(QWidget):
         s_manip = self.my_s_combo_box.current_text
         x = int(self.my_x_le.text)
         y = int(self.my_y_le.text)
-        place_image(bg,subject,bg_manip,s_manip,x,y)
+        bg_w = int(self.my_bg_w_le.text)
+        bg_h = int(self.my_bg_w_le.text)
+        s_w = int(self.my_bg_w_le.text)
+        s_h = int(self.my_bg_w_le.text)
+        place_image(bg,subject,bg_manip,s_manip,x,y,bg_w,bg_h,s_w,s_h)
 
 
 
@@ -99,7 +132,7 @@ class ResultWindow(QWidget):
         self.set_layout(self.layout)
         self.show()
 
-def place_image(bg,subject,bg_manip,s_manip,x,y):
+def place_image(bg,subject,bg_manip,s_manip,x,y,bg_w,bg_h,s_w,s_h):
     return
 
 
