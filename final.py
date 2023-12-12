738f451aba7fe28afe814eea71878eb40499dc62
Michael <miconley@csumb.edu>
2023-12-12 14:38:14 -0800

    testing web scraping integration, manipulation not done, all else done

--- final.py
+++ final.py
@@ -3,11 +3,11 @@
 #Date: 12/4/2023
 #Description: Creates a window that takes 2 file names, an xy coordinate, sizes for both images, and manipulation choice, then places the second image on the first according to the information given.
 import sys
-from PySide6.QtWidgets import (QWidget, QApplication, QLabel, QVBoxLayout, QLineEdit, QComboBox, QPushButton)
+from PySide6.QtWidgets import (QWidget, QApplication, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit, QComboBox, QPushButton)
 from PySide6.QtCore import Slot
 from __feature__ import snake_case, true_property
 from PySide6.QtGui import QPixmap
-from placeimage import place_image
+from placeimage import place_image,resize
 
 from PIL import Image
 
@@ -36,7 +36,7 @@ class MyWindow(QWidget):
         self.my_y_le.minimum_width = 250
         #The image will be placed with the top left corner on the given pixel
 
-        self.my_list = ["None", "Sepia", "Negative", "Grayscale", "Thumbnail", "Chromatic Abberation"]
+        self.my_list = ["None", "Sepia", "Negative", "Grayscale", "Chromatic Abberation"]
 
         self.bg_cb_lbl = QLabel('Select background manipulation:')
         self.my_bg_combo_box = QComboBox()
@@ -63,13 +63,13 @@ class MyWindow(QWidget):
         self.my_s_h_le.minimum_width = 250
         #negative sizes will mean the image will be flipped
 
-        self.blend_list = ["Linear","Outward","Inward"]
+        self.blend_list = ["Linear","Outward","Inward","Wave"]
 
         self.blend_cb_lbl = QLabel('Enter edge blend type: ')
         self.my_blend_combo_box = QComboBox()
         self.my_blend_combo_box.add_items(self.blend_list)
 
-        self.b_s_le_lbl = QLabel('Enter edge blend srength:')
+        self.b_s_le_lbl = QLabel('Enter edge blend strength:')
         self.my_b_s_le = QLineEdit("0")
         self.my_b_s_le.minimum_width = 250
 
@@ -78,43 +78,135 @@ class MyWindow(QWidget):
         self.c_s_le_lbl = QLabel('Enter corner style:')
         self.my_corner_combo_box = QComboBox()
         self.my_corner_combo_box.add_items(self.corner_list)
+
+        self.bg_t_c_le_lbl = QLabel('Pixels to crop from top of background:')
+        self.my_bg_t_c_le = QLineEdit("0")
+        self.my_bg_t_c_le.minimum_width = 250
+
+        self.bg_b_c_le_lbl = QLabel('Pixels to crop from bottom of background:')
+        self.my_bg_b_c_le = QLineEdit("0")
+        self.my_bg_b_c_le.minimum_width = 250
+
+        self.bg_l_c_le_lbl = QLabel('Pixels to crop from left side of background:')
+        self.my_bg_l_c_le = QLineEdit("0")
+        self.my_bg_l_c_le.minimum_width = 250
+
+        self.bg_r_c_le_lbl = QLabel('Pixels to crop from right of background:')
+        self.my_bg_r_c_le = QLineEdit("0")
+        self.my_bg_r_c_le.minimum_width = 250
+
+        self.s_t_c_le_lbl = QLabel('Pixels to crop from top of subject:')
+        self.my_s_t_c_le = QLineEdit("0")
+        self.my_s_t_c_le.minimum_width = 250
+
+        self.s_b_c_le_lbl = QLabel('Pixels to crop from bottom of subject:')
+        self.my_s_b_c_le = QLineEdit("0")
+        self.my_s_b_c_le.minimum_width = 250
+
+        self.s_l_c_le_lbl = QLabel('Pixels to crop from left side of subject:')
+        self.my_s_l_c_le = QLineEdit("0")
+        self.my_s_l_c_le.minimum_width = 250
+
+        self.s_r_c_le_lbl = QLabel('Pixels to crop from right of subject:')
+        self.my_s_r_c_le = QLineEdit("0")
+        self.my_s_r_c_le.minimum_width = 250
         
         my_btn = QPushButton("Submit")
-
         my_btn.clicked.connect(self.on_submit)
 
-        vbox.add_widget(self.bg_le_lbl)
-        vbox.add_widget(self.my_bg_le)
-        vbox.add_widget(self.s_le_lbl)
-        vbox.add_widget(self.my_s_le)
+        hbox = QHBoxLayout()
+        bg_box = QVBoxLayout()
+        s_box = QVBoxLayout()
+        bg_select_box = QHBoxLayout()
+        s_select_box = QHBoxLayout()
+
+        bg_left_btn = QPushButton("<")
+        bg_right_btn = QPushButton(">")
+        s_left_btn = QPushButton("<")
+        s_right_btn = QPushButton(">")
+        
+
+        bg_select_box.add_widget(bg_left_btn)
+        bg_select_box.add_widget(bg_right_btn)
+        s_select_box.add_widget(s_left_btn)
+        s_select_box.add_widget(s_right_btn)
+        
+
+
+        bg_box.add_widget(self.bg_le_lbl)
+        bg_box.add_widget(self.my_bg_le)
+        bg_box.add_layout(bg_select_box)
+        s_box.add_widget(self.s_le_lbl)
+        s_box.add_widget(self.my_s_le)
+        s_box.add_layout(s_select_box)
         vbox.add_widget(self.x_le_lbl)
         vbox.add_widget(self.my_x_le)
         vbox.add_widget(self.y_le_lbl)
         vbox.add_widget(self.my_y_le)
-        vbox.add_widget(self.bg_cb_lbl)
-        vbox.add_widget(self.my_bg_combo_box)
-        vbox.add_widget(self.s_cb_lbl)
-        vbox.add_widget(self.my_s_combo_box)
-        vbox.add_widget(self.bg_w_le_lbl)
-        vbox.add_widget(self.my_bg_w_le)
-        vbox.add_widget(self.bg_h_le_lbl)
-        vbox.add_widget(self.my_bg_h_le)
-        vbox.add_widget(self.s_w_le_lbl)
-        vbox.add_widget(self.my_s_w_le)
-        vbox.add_widget(self.s_h_le_lbl)
-        vbox.add_widget(self.my_s_h_le)
+        bg_box.add_widget(self.bg_cb_lbl)
+        bg_box.add_widget(self.my_bg_combo_box)
+        s_box.add_widget(self.s_cb_lbl)
+        s_box.add_widget(self.my_s_combo_box)
+        bg_box.add_widget(self.bg_w_le_lbl)
+        bg_box.add_widget(self.my_bg_w_le)
+        bg_box.add_widget(self.bg_h_le_lbl)
+        bg_box.add_widget(self.my_bg_h_le)
+        s_box.add_widget(self.s_w_le_lbl)
+        s_box.add_widget(self.my_s_w_le)
+        s_box.add_widget(self.s_h_le_lbl)
+        s_box.add_widget(self.my_s_h_le)
         vbox.add_widget(self.blend_cb_lbl)
         vbox.add_widget(self.my_blend_combo_box)
         vbox.add_widget(self.b_s_le_lbl)
         vbox.add_widget(self.my_b_s_le)
         vbox.add_widget(self.c_s_le_lbl)
         vbox.add_widget(self.my_corner_combo_box)
+        bg_box.add_widget(self.bg_t_c_le_lbl)
+        bg_box.add_widget(self.my_bg_t_c_le)
+        bg_box.add_widget(self.bg_b_c_le_lbl)
+        bg_box.add_widget(self.my_bg_b_c_le)
+        bg_box.add_widget(self.bg_l_c_le_lbl)
+        bg_box.add_widget(self.my_bg_l_c_le)
+        bg_box.add_widget(self.bg_r_c_le_lbl)
+        bg_box.add_widget(self.my_bg_r_c_le)
+        s_box.add_widget(self.s_t_c_le_lbl)
+        s_box.add_widget(self.my_s_t_c_le)
+        s_box.add_widget(self.s_b_c_le_lbl)
+        s_box.add_widget(self.my_s_b_c_le)
+        s_box.add_widget(self.s_l_c_le_lbl)
+        s_box.add_widget(self.my_s_l_c_le)
+        s_box.add_widget(self.s_r_c_le_lbl)
+        s_box.add_widget(self.my_s_r_c_le)
 
+        bg_display_box = QVBoxLayout()
+        self.bg_d_lbl = QLabel('Background preview:')
+        bg_display_box.add_widget(self.bg_d_lbl)
+        s_display_box = QVBoxLayout()
+        self.s_d_lbl = QLabel('Subject preview:')
+        s_display_box.add_widget(self.s_d_lbl)
+
+        bg_label = QLabel()
+        bg_pixmap = QPixmap('finalimages/background.jpg')
+        bg_label.pixmap = bg_pixmap
+        bg_display_box.add_widget(bg_label)
+        
+        s_label = QLabel()
+        s_pixmap = QPixmap('finalimages/subject.jpg')
+        s_label.pixmap = s_pixmap
+        s_display_box.add_widget(s_label)
+
+        hbox.add_layout(bg_display_box)
+        hbox.add_layout(bg_box)
+        hbox.add_layout(s_box)
+        hbox.add_layout(s_display_box)
+        vbox.add_layout(hbox)
         vbox.add_widget(my_btn)
 
         self.set_layout(vbox)
 
         self.show()
+    
+    
 
     @Slot()
     def on_submit(self):
@@ -131,7 +223,15 @@ class MyWindow(QWidget):
         blend_style = self.my_blend_combo_box.current_text
         blend_strength = int(self.my_b_s_le.text)
         corner = self.my_corner_combo_box.current_text
-        place_image(bg,subject,bg_manip,s_manip,x_pos,y_pos,bg_w,bg_h,s_w,s_h,blend_style,blend_strength,corner)
+        bg_t = int(self.my_bg_t_c_le.text)
+        bg_b = int(self.my_bg_b_c_le.text)
+        bg_l = int(self.my_bg_l_c_le.text)
+        bg_r = int(self.my_bg_r_c_le.text)
+        s_t = int(self.my_s_t_c_le.text)
+        s_b = int(self.my_s_b_c_le.text)
+        s_l = int(self.my_s_l_c_le.text)
+        s_r = int(self.my_s_r_c_le.text)
+        place_image(bg,subject,bg_manip,s_manip,x_pos,y_pos,bg_w,bg_h,s_w,s_h,blend_style,blend_strength,corner,bg_t,bg_b,bg_l,bg_r,s_t,s_b,s_l,s_r)
         rw = ResultWindow()
 
 
--- placeimage.py
+++ placeimage.py
@@ -1,16 +1,16 @@
 from PIL import Image
 import math
-def place_image(bg,subject,bg_manip,s_manip,x_pos,y_pos,bg_w,bg_h,s_w,s_h,b_style,b_strength,corner):
+def place_image(bg,subject,bg_manip,s_manip,x_pos,y_pos,bg_w,bg_h,s_w,s_h,b_style,b_strength,corner,bg_t,bg_b,bg_l,bg_r,s_t,s_b,s_l,s_r):
     bg_im = Image.open(bg)
     s_im = Image.open(subject)
     if(s_w == 0):
-        s_w = s_im.width
+        s_w = s_im.width - s_l - s_r
     if(s_h == 0):
-        s_h = s_im.height
+        s_h = s_im.height - s_t - s_b
     if(bg_w == 0):
-        bg_w = bg_im.width
+        bg_w = bg_im.width - bg_l - bg_r
     if(bg_h == 0):
-        bg_h = bg_im.height
+        bg_h = bg_im.height - bg_t - bg_b
 
     if(x_pos == -1):
         x_pos = (bg_w//2)-(s_w//2)
@@ -21,19 +21,19 @@ def place_image(bg,subject,bg_manip,s_manip,x_pos,y_pos,bg_w,bg_h,s_w,s_h,b_styl
     if(y_pos == -2):
         y_pos == bg_h - s_h
 
-    bg_im = manipulate(bg_manip,resize(bg_im,bg_w,bg_h))
+    bg_im = manipulate(bg_manip,crop(resize(bg_im,bg_w,bg_h),bg_t,bg_b,bg_l,bg_r))
     s_im = manipulate(s_manip,resize(s_im,s_w,s_h))
-    for y in range(0,s_h):
+    for y in range(0,s_h-s_t-s_b):
         if y + y_pos < 0:
             continue
         if y + y_pos >= bg_im.height:
             break
-        for x in range(0,s_w):
+        for x in range(0,s_w-s_l-s_r):
             if x + x_pos < 0:
                 continue
             if x + x_pos >= bg_im.width:
                 break
-            bg_im.putpixel((x+x_pos,y+y_pos),s_im.getpixel((x,y)))
+            bg_im.putpixel((x+x_pos-bg_l,y+y_pos-bg_t),s_im.getpixel((x+s_l,y+s_t)))
         
     left = x_pos
     right = x_pos + s_w - 1
@@ -71,10 +71,22 @@ def place_image(bg,subject,bg_manip,s_manip,x_pos,y_pos,bg_w,bg_h,s_w,s_h,b_styl
 
                 pixel = [0,0,0]
                 foo = (baseline + b_strength - m)/b_strength
-                bar = -(foo-0.5)+0.5
+                
                 if(foo > 1):
-                    bar = (baseline - m)/b_strength
-                    foo = -(bar-0.5)+0.5
+                    foo = (baseline - m)/b_strength
+                    foo = -(foo-0.5)+0.5
+                    
+                bar = -(foo-0.5)+0.5
+                if(b_style == "Outward"):
+                    foo = math.sqrt(abs(pow(b_strength,2) - pow(m-baseline,2))/pow(b_strength,2))
+                    bar = -(foo-0.5)+0.5
+                if(b_style == "Inward"):
+                    foo = math.sqrt(abs(pow(b_strength,2) - pow(b_strength-abs(m-baseline),2))/pow(b_strength,2))
+                    foo = -(foo-0.5)+0.5
+                    bar = -(foo-0.5)+0.5
+                if(b_style == "Wave"):
+                    foo = math.sin((abs(m-baseline)*math.pi/b_strength)+math.pi/2)*0.5+0.5
+                    bar = -(foo-0.5)+0.5
                 for n in range(0,3):
                     pixel[n] = int(bar*bg_im.getpixel((x,y))[n] + (bg_im.getpixel(base)[n]*foo))
                 pixel = (pixel[0],pixel[1],pixel[2])
@@ -91,7 +103,7 @@ def place_image(bg,subject,bg_manip,s_manip,x_pos,y_pos,bg_w,bg_h,s_w,s_h,b_styl
                     continue
                 pixel = [0,0,0]
                 if(corner == "Diagonal"):
-                    bar = (abs(x - base[0]) + abs(y - base[1]))/b_strength
+                    bar = (abs(x - base[0] - x_direction) + abs(y - base[1]- y_direction))/b_strength
                 if(corner == "Rounded"):
                     bar = (math.sqrt(pow(x-base[0],2)+pow(y-base[1],2)))/b_strength
                 if(corner == "Sharp"):
@@ -103,6 +115,14 @@ def place_image(bg,subject,bg_manip,s_manip,x_pos,y_pos,bg_w,bg_h,s_w,s_h,b_styl
                         bar = -(bar-0.5)+0.5
                 if bar > 1:
                     bar = 1
+                if(b_style == "Outward"):
+                    bar = -(bar-0.5)+0.5
+                    bar = math.sqrt(1-math.pow(1-bar,2))
+                    bar = -(bar-0.5)+0.5
+                if(b_style == "Inward"):
+                    bar = math.sqrt(1-math.pow(1-bar,2))
+                if(b_style == "Wave"):
+                    bar = math.sin((1-bar+0.5)*math.pi)*0.5+0.5
                 foo = -(bar-0.5)+0.5
                 for n in range(0,3):
                     pixel[n] = int(bar*bg_im.getpixel((x,y))[n] + (bg_im.getpixel(base)[n]*foo))
@@ -116,9 +136,15 @@ def manipulate(manip,im):
     return im
 
 def resize(im,w,h):
-    print("resizing")
     out = Image.new("RGB",(w,h))
     for y in range(0,h):
         for x in range(0,w):
             out.putpixel((x,y),im.getpixel((x*im.width/w,y*im.height/h)))
+    return out
+
+def crop(im,top,bottom,left,right):
+    out = Image.new("RGB",(im.width-left-right,im.height-top-bottom))
+    for y in range(0,out.height):
+        for x in range(0,out.width):
+            out.putpixel((x,y),im.getpixel((left+x,top+y)))
     return out
